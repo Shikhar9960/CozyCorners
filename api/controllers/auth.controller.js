ddd2351112ed77ba68524a6528aa60c546248cba
Shikhar Jaiswal <144546090+Shikhar9960@users.noreply.github.com>
2025-01-16 16:01:49 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -17,24 +17,15 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
-
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found!'));
-
-    const validPassword = await bcryptjs.compare(password, validUser.password);
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
-
     res
-      .cookie('access_token', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'Strict',
-      })
+      .cookie('access_token', token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
@@ -42,19 +33,14 @@ export const signin = async (req, res, next) => {
   }
 };
 
-
 export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
-      return res
-        .cookie('access_token', token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'Strict',
-        })
+      res
+        .cookie('access_token', token, { httpOnly: true })
         .status(200)
         .json(rest);
     } else {
@@ -62,34 +48,19 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-
-      const usernameBase = req.body.name.split(' ').join('').toLowerCase();
-      let username = usernameBase + Math.random().toString(36).slice(-4);
-
-      // Ensure username is unique
-      let userExists = await User.findOne({ username });
-      while (userExists) {
-        username = usernameBase + Math.random().toString(36).slice(-4); // Regenerate username if it already exists
-        userExists = await User.findOne({ username });
-      }
-
       const newUser = new User({
-        username,
+        username:
+          req.body.name.split(' ').join('').toLowerCase() +
+          Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
         avatar: req.body.photo,
       });
       await newUser.save();
-
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
-
       res
-        .cookie('access_token', token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'Strict',
-        })
+        .cookie('access_token', token, { httpOnly: true })
         .status(200)
         .json(rest);
     }
@@ -97,7 +68,6 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
-
 
 export const signOut = async (req, res, next) => {
   try {
